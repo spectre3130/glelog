0edb9f73de51148506cb0c3208de07b27ee9da5d
Hyeonho Lee <flqktm123@gmail.com>
2020-03-06 17:04:39 +0900

    fix preview css

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 const prod = process.env.NODE_ENV === 'prod';
 
-mongoose.connect(prod ? process.env.DB_HOST : 'mongodb://localhost:27017/glelog', { 
+mongoose.connect(prod ? process.env.DB_HOST : process.env.DB_TEST, { 
     useCreateIndex: true, 
     useUnifiedTopology: true, 
     useNewUrlParser: true, 
